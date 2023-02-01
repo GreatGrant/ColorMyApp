f5e24b8b084a02bb5aa54a66c3440af739f79044
Great Grant <greatgrant116@gmail.com>
2023-02-01 11:45:22 +0000

    migrate from android extensions to viewBinding

--- app/src/main/java/com/gralliams/colormyviews/MainActivity.kt
+++ app/src/main/java/com/gralliams/colormyviews/MainActivity.kt
@@ -4,20 +4,24 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import kotlinx.android.synthetic.main.activity_main.*
+import com.gralliams.colormyviews.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        var view = binding.root
+        setContentView(view)
         setListeners()
     }
 
     private fun setListeners(){
         val clickableViews: List<View> =
-            listOf(box_one_text, box_two_text, box_three_text,
-                box_four_text, box_five_text, constraint_layout,
-                red_button, green_button, yellow_button)
+            listOf(binding.boxOneText, binding.boxTwoText, binding.boxThreeText,
+                binding.boxFourText, binding.boxFiveText, binding.constraintLayout,
+                binding.redButton, binding.greenButton, binding.yellowButton)
 
         for (view in clickableViews){
             view.setOnClickListener { makeColored(it) }
@@ -37,9 +41,9 @@ class MainActivity : AppCompatActivity() {
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
 
             // Boxes using custom colors for background
-            R.id.red_button -> box_three_text.setBackgroundResource(R.color.red)
-            R.id.yellow_button -> box_five_text.setBackgroundResource(R.color.green)
-            R.id.green_button -> box_four_text.setBackgroundResource(R.color.yellow)
+            R.id.red_button -> binding.boxThreeText.setBackgroundResource(R.color.red)
+            R.id.yellow_button -> binding.boxFiveText.setBackgroundResource(R.color.green)
+            R.id.green_button -> binding.boxFourText.setBackgroundResource(R.color.yellow)
 
 
             else -> view.setBackgroundColor(Color.LTGRAY)
